1c4209918011988d6709f7a976476564f255317e
DanceMore <dancemore@protonmail.com>
2023-02-08 21:43:27 -0800

    macos quickstart from ~/

---
+++ macos-quickstart.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+brew cask install google-chrome
+brew cask install firefox
+brew cask install vlc
+brew cask install keepassx
+brew cask install signal
+brew cask install telegram
+brew cask install minecraft
+brew install opensc
